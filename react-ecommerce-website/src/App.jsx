c6c50903e3f951b200a31349fc0dccb40c1e4a42
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2024-02-08 10:44:17 +0700

    navbar

--- react-ecommerce-website/src/App.jsx
+++ react-ecommerce-website/src/App.jsx
@@ -1,11 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       {/* Chứa những cái children còn lại */}
-      <nav>Nav</nav>
+      <Navbar />
       <Outlet />
     </>
   );
---
+++ react-ecommerce-website/src/components/Navbar.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { FaBars, FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
+import { FaBagShopping } from "react-icons/fa6";
+import logo from "../../public/logo.png";
+import { Link } from "react-router-dom";
+import { IoMdClose } from "react-icons/io";
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const navItems = [
+    { title: "Jewelry & Accessories", path: "/" },
+    { title: "Clothing & Shoes", path: "/" },
+    { title: "Home & Living", path: "/" },
+    { title: "Wedding & Party", path: "/" },
+    { title: "Toys & Entertainment", path: "/" },
+    { title: "Art & Collectibles", path: "/" },
+    { title: "Craft Supplies & Tools", path: "/" },
+  ];
+  return (
+    <header className="max-w-screen-2xl xl:px-28 px-4">
+      <nav className="flex items-center justify-between container md:py-4 pt-6 pb-3">
+        <FaSearch className="text-Black w-5 h-5 cursor-pointer hidden md:block " />
+        <a href="/">
+          {/* logo */}
+          <img src={logo} alt="" />
+        </a>
+
+        {/* account and shopping btn */}
+        <div className="text-lg text-Black sm:flex items-center gap-4 hidden">
+          <a href="" className="flex items-center gap-2">
+            <FaUser /> Account
+          </a>
+          <a href="" className="flex items-center gap-2">
+            <FaBagShopping /> Shopping
+          </a>
+        </div>
+        {/* navbar for sm devices */}
+        <div className="sm:hidden">
+          <button onClick={toggleMenu}>
+            {isMenuOpen ? (
+              <IoMdClose className="w-5 h-5  text-Black" />
+            ) : (
+              <FaBars className="w-5 h-5  text-Black" />
+            )}
+          </button>
+        </div>
+      </nav>
+      <hr />
+      {/* category items */}
+      <div className="pt-4">
+        <ul className="lg:flex items-center justify-between text-Black hidden">
+          {navItems.map(({ title, path }) => (
+            <li key={title} className="hover:text-orange-500">
+              <Link to="/">{title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* mobile menu */}
+      {isMenuOpen && (
+        <ul className="bg-Black text-white px-4 py-2 rounded">
+          {navItems.map(({ title, path }) => (
+            <li
+              key={title}
+              className="hover:text-orange-500 my-3 cursor-pointer"
+            >
+              <Link to="/">{title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </header>
+  );
+};
+
+export default Navbar;
