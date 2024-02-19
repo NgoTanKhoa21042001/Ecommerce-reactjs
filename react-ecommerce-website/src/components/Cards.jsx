71d19f6fce7665b3d55f2b9508803d5a824b8979
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2024-02-19 16:51:17 +0700

    collection section

--- react-ecommerce-website/src/components/Cards.jsx
+++ react-ecommerce-website/src/components/Cards.jsx
@@ -15,7 +15,7 @@ const Cards = ({ filteredItems }) => {
           </Link>
           <div className="mt-4 px-4">
             <h4 className="text-base font-semibold mb-2">{item.title}</h4>
-            <div className="">
+            <div>
               <p className="text-black/50">{item.category}</p>
               <p className="font-semibold">{item.price}</p>
             </div>
--- react-ecommerce-website/src/pages/home/Home.jsx
+++ react-ecommerce-website/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Banner from "./Banner";
 import Category from "./Category";
 import Products from "./Products";
+import Collection from "./Collection";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Banner />
       <Category />
       <Products />
+      <Collection />
     </div>
   );
 };
